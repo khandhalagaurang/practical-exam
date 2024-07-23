1a5063fff2911b9c09d379926cdf0092f074427f
Khandhala Gaurang <149217937+khandhalagaurang@users.noreply.github.com>
2024-07-23 14:25:08 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 import { useState } from 'react'
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 import './App.css'
 import Input from './Redux/Input'
 import Display from './Redux/Display'
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import Navbar from './component/Navbar';
-import RoomList from './component/RoomList';
-import RoomDetail from './component/RoomDetail';
-import ReservationForm from './component/ReservationForm';
-import ReservationList from './component/ReservationList';
-
-
 import GoogleAuth from './GoogleAuth'
 
 function App() {
@@ -21,23 +9,12 @@ function App() {
 
   return (
     <>
-    
-         <div className="Main-Cont">
-        <div className='Full'>
-          <div className="Navbar">
-             <Navbar />          </div>
-          <Routes>
-            <Route path='/' element={<RoomList />} />
-            <Route path='/Roomdetail' element={<RoomDetail />} />
-            <Route path='/Reservationform' element={<ReservationForm />} />
-            <Route path='/Reservationlist' element={<ReservationList />} />
-          </Routes>
-        </div>
-      </div>
-      <Input/>
-      <Display/>
 
-      <GoogleAuth/>
+
+      <Input />
+      <Display />
+
+      <GoogleAuth />
 
       {/* json server link
       http://localhost:5000/student */}
